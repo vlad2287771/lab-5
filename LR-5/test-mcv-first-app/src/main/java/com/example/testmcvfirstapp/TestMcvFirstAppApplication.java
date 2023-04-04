ada5099cd66958cda66bf74b36e4bed161fb41e8
vlad2287771 <101133573+vlad2287771@users.noreply.github.com>
2023-04-04 12:12:46 +0500

    Add files via upload

---
+++ LR-5/test-mcv-first-app/src/main/java/com/example/testmcvfirstapp/TestMcvFirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmcvfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMcvFirstAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMcvFirstAppApplication.class, args);
+    }
+
+}
---
+++ LR-5/test-mcv-first-app/src/test/java/com/example/testmcvfirstapp/TestMcvFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testmcvfirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMcvFirstAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
